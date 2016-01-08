679d114f338bbdf8532504e53b2dad2788297d68
xtotdam <xtotdam@yandex.ru>
2016-01-08 22:44:34 +0300

    add double quotes to arguments

--- - PDF, DjVu/Split djvu.sh
+++ - PDF, DjVu/Split djvu.sh
@@ -4,9 +4,9 @@ IN=$1
 NAME=${IN%.*}
 RND=`date | md5sum | tr -d ' -'`
 
-ddjvu -format=pdf $1 $RND.pdf
-mv $1 $NAME.old.djvu
-python2 "/home/xtotdam/.local/share/nautilus/scripts/- Split pages/un2up.py" <$RND.pdf >$RND.temp.pdf
-#rm -v $RND.pdf
-pdf2djvu --lossy -j0 $RND.temp.pdf >$NAME.djvu
-#rm -v $RND.temp.pdf
+ddjvu -format=pdf "$1" "$RND.pdf"
+mv "$1" "$NAME.old.djvu"
+python2 "/home/xtotdam/.local/share/nautilus/scripts/- Split pages/un2up.py" <"$RND.pdf" >"$RND.temp.pdf"
+#rm -v "$RND.pdf"
+pdf2djvu --lossy -j0 "$RND.temp.pdf" >"$NAME.djvu"
+#rm -v "$RND.temp.pdf"
--- - PDF, DjVu/Split pdf.sh
+++ - PDF, DjVu/Split pdf.sh
@@ -3,5 +3,5 @@
 IN=$1
 NAME=${IN%.*}
 
-mv $1 $NAME.old.pdf
-python2 "/home/xtotdam/.local/share/nautilus/scripts/- Split pages/un2up.py" <$NAME.old.pdf >$NAME.pdf
+mv "$1" "$NAME.old.pdf"
+python2 "/home/xtotdam/.local/share/nautilus/scripts/- Split pages/un2up.py" <"$NAME.old.pdf" >"$NAME.pdf"
